32c954fab6aa2e9f9564f89b0f9ea0d0add91174
liulu <541877028@qq.com>
2020-02-20 22:59:47 +0800

    一些优化

--- pages/index/category/_id.vue
+++ pages/index/category/_id.vue
@@ -44,6 +44,6 @@ export default {
   },
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/assets/components/slogin.scss';
 </style>
--- pages/index/search.vue
+++ pages/index/search.vue
@@ -94,6 +94,6 @@ export default {
   },
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/assets/components/slogin.scss';
 </style>
